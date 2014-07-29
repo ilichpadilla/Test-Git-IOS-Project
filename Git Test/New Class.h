91454647fb8a8973bb74fc05e57fc14d78ac096e
Ilich Padilla <ilichpadilla@tdsdq-pd-01.sdq.theoreminc.net>
2014-07-29 11:47:15 -0400

    added new class

---
+++ Git Test/New Class.h
@@ -0,0 +1,13 @@
+//
+//  New Class.h
+//  Git Test
+//
+//  Created by Ilich Padilla on 7/29/14.
+//  Copyright (c) 2014 Ilich Padilla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface New_Class : NSObject
+
+@end
---
+++ Git Test/New Class.m
@@ -0,0 +1,13 @@
+//
+//  New Class.m
+//  Git Test
+//
+//  Created by Ilich Padilla on 7/29/14.
+//  Copyright (c) 2014 Ilich Padilla. All rights reserved.
+//
+
+#import "New Class.h"
+
+@implementation New_Class
+
+@end
