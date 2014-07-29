1a326fce984d925be568639820ad8590b99cc9df
Ilich Padilla <ilichpadilla@tdsdq-pd-01.sdq.theoreminc.net>
2014-07-29 11:38:03 -0400

    added a test class

---
+++ Git Test/Test.h
@@ -0,0 +1,13 @@
+//
+//  Test.h
+//  Git Test
+//
+//  Created by Ilich Padilla on 7/29/14.
+//  Copyright (c) 2014 Ilich Padilla. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface Test : NSObject
+
+@end
---
+++ Git Test/Test.m
@@ -0,0 +1,13 @@
+//
+//  Test.m
+//  Git Test
+//
+//  Created by Ilich Padilla on 7/29/14.
+//  Copyright (c) 2014 Ilich Padilla. All rights reserved.
+//
+
+#import "Test.h"
+
+@implementation Test
+
+@end
